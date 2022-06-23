686290a7f7265b2593f9db9b514ca35c2749e9ec
Subin-44888 <subindavid605@gmail.com>
2022-06-23 23:43:58 +0530

    admin partial build

--- build/app/generated/source/buildConfig/release/com/example/tutory/BuildConfig.java
+++ build/app/generated/source/buildConfig/release/com/example/tutory/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.example.tutory";
   public static final String BUILD_TYPE = "release";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.0";
+  public static final String VERSION_NAME = "1.0.0";
 }
